4238ed7b11bf0745a0a1d217ddc62d9e8241b7a8
Vince <vince@krijnsen.com>
2023-11-11 06:52:10 +0000

    fix: off-by-one error; add: exit option

--- src/main/java/LibrarySystem/interactor/MainMenu.java
+++ src/main/java/LibrarySystem/interactor/MainMenu.java
@@ -7,7 +7,8 @@ public class MainMenu extends Interaction {
     String header = "MAIN MENU\n";
     MenuItem[] menu = {
             new MenuItem("Add an author", "add-author"),
-            new MenuItem("Add an asset", "add-asset")
+            new MenuItem("Add an asset", "add-asset"),
+            new MenuItem("Exit", "exit")
     };
     String prompt = "Please make your choice: ";
 
@@ -22,6 +23,6 @@ public class MainMenu extends Interaction {
 
         int choice = StandardInput.getPositiveNumber(prompt, menu.length);
 
-        nextReference = menu[choice].menuReference();
+        nextReference = menu[--choice].nextInteractionReference();
     }
 }
