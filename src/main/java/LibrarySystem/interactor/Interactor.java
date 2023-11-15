db5636c7991f2171a4a20ebe927eb28cc5897cd9
Vince <vince@krijnsen.com>
2023-11-15 12:11:27 +0000

    extract methods to create menus

--- src/main/java/LibrarySystem/interactor/Interactor.java
+++ src/main/java/LibrarySystem/interactor/Interactor.java
@@ -16,40 +16,60 @@ class Interactor {
 
     private HashMap<String, Interaction> loadAllInteractions() {
         var allInteractions = new HashMap<String, Interaction>();
-        allInteractions.put("main", new Menu("MAIN MENU\n", new MenuItem[] {
+        allInteractions.put("main", createMainMenu());
+        allInteractions.put("common-operations", createOperationsMenu());
+        allInteractions.put("common-filters", createFiltersMenu());
+        allInteractions.put("manage-catalogue", createCatalogueMenu());
+        allInteractions.put("manage-users", createUserMenu());
+        allInteractions.put("add-asset", new addAsset());
+        allInteractions.put("add-author", new AddAuthor());
+        return allInteractions;
+    }
+
+    private Menu createMainMenu() {
+        return new Menu("MAIN MENU\n", new MenuItem[] {
                 new MenuItem("OPERATIONS", "common-operations"),
                 new MenuItem("LISTS", "common-filters"),
                 new MenuItem("CATALOGUE management", "manage-catalogue"),
                 new MenuItem("USER management", "manage-users"),
                 new MenuItem("Exit", "exit")
-        }));
-        allInteractions.put("common-operations", new Menu("OPERATIONS\n", new MenuItem[] {
+        });
+    }
+
+    private Menu createOperationsMenu() {
+        return new Menu("OPERATIONS\n", new MenuItem[] {
                 new MenuItem("BORROW asset", "borrow-asset"),
                 new MenuItem("RETURN asset", "return-asset"),
                 new MenuItem("Back", "main")
-        }));
-        allInteractions.put("common-filters", new Menu("LISTS\n", new MenuItem[] {
+        });
+    }
+
+    private Menu createFiltersMenu() {
+        return new Menu("LISTS\n", new MenuItem[] {
                 new MenuItem("all BORROWED", "all-borrowed"),
                 new MenuItem("CREATED BY", "created-by"),
                 new MenuItem("BORROWED BY", "borrowed-by"),
                 new MenuItem("ALL OVERDUE", "all-overdue"),
                 new MenuItem("ALL assets", "all-assets"),
                 new MenuItem("Back", "main")
-        }));
-        allInteractions.put("manage-catalogue", new Menu("MANAGE ASSETS\n", new MenuItem[]{
+        });
+    }
+
+    private Menu createCatalogueMenu() {
+        return new Menu("MANAGE ASSETS\n", new MenuItem[]{
                 new MenuItem("ADD asset", "add-asset"),
                 new MenuItem("REMOVE asset", "remove-asset"),
                 new MenuItem("add AUTHOR", "add-author"),
                 new MenuItem("Back", "main")
-        }));
-        allInteractions.put("manage-users", new Menu("USER MENU\n", new MenuItem[]{
+        });
+    }
+
+    private Menu createUserMenu() {
+        return new Menu("USER MENU\n", new MenuItem[]{
                 new MenuItem("ADD customer", "add-customer"),
                 new MenuItem("DEACTIVATE customer", "deactivate-customer"),
                 new MenuItem("Back", "main")
-        }));
-        allInteractions.put("add-asset", new addAsset());
-        allInteractions.put("add-author", new AddAuthor());
-        return allInteractions;
+        });
     }
 
     public void completeRequestResponseCycle(Library library) {
