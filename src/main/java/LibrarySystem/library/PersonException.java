6c7bda0bb36fd70ac9b917feddcc6d301cae30c6
eman-mk <118221635+eman-mk@users.noreply.github.com>
2023-11-16 10:59:08 +0000

    Add files via upload

---
+++ src/main/java/LibrarySystem/library/PersonException.java
@@ -0,0 +1,12 @@
+package LibrarySystem.library;
+
+public class PersonException extends Exception {
+    public PersonException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
