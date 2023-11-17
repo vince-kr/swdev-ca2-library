bb888466a69151119092e581fa11449c4097a6b6
Dee68 <ddimie283@gmail.com>
2023-11-17 00:20:09 +0000

    Add partial tests to models.

---
+++ src/test/java/LibrarySystem/library/catalogue/AuthorTest.java
@@ -0,0 +1,31 @@
+package LibrarySystem.library.catalogue;
+
+import LibrarySystem.library.PersonException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorTest {
+    private Author author;
+    private BookAudioBook book;
+    @BeforeEach
+    void setUp() throws PersonException {
+        try {
+            author = new Author(1,"King James");
+        } catch (PersonException e) {
+            throw new RuntimeException(e);
+        }
+        book = new BookAudioBook("Holy Bible","0-438-98147-1","1999",author,1);
+    }
+
+    @Test
+    void getBooksAuthored() {
+        ArrayList<BookAudioBook> books = new ArrayList<>();
+        books.add(book);
+        author.setBooksAuthored(books);
+        assertEquals(1,author.getBooksAuthored().size());
+    }
+}
---
+++ src/test/java/LibrarySystem/library/catalogue/BookAudioBookTest.java
@@ -0,0 +1,54 @@
+package LibrarySystem.library.catalogue;
+
+import LibrarySystem.library.PersonException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookAudioBookTest {
+
+    private BookAudioBook book;
+    private Author author;
+    @BeforeEach
+    void setUp() throws PersonException {
+        author = new Author(1,"king James");
+        book = new BookAudioBook("Holy Bible","0-546-77123-9","2000",author,1);
+    }
+
+    @Test
+    void getQuantity() {
+        book.setQuantity(10);
+        assertEquals(10, book.getQuantity());
+    }
+
+    @Test
+    void getDateIssued() {
+    }
+
+    @Test
+    void getDateDue() {
+    }
+
+    @Test
+    void getOverDue() {
+        book.setOverDue("OverDue");
+        assertEquals("OverDue".length(), book.getOverDue().length());
+    }
+
+    @Test
+    void getIsbn() {
+    }
+
+    @Test
+    void getPublishedYear() {
+        book.setPublishedYear("1998");
+        assertEquals(Integer.parseInt("1998"),Integer.parseInt(book.getPublishedYear()));
+    }
+
+    @Test
+    void getAuthor() {
+        book.setAuthor(author);
+        assertEquals(author, book.getAuthor());
+    }
+}
